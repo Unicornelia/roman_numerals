aa293a7f06cf4ff76738d41560094a7944cec807
unicornelia <ez.kornelia@gmail.com>
2016-11-15 09:41:44 +0000

    Created and updated readme for roman numerals

---
+++ lib/roman_numerals.rb
@@ -0,0 +1,6 @@
+class Numerals
+
+ROMAN_NUMERALS = {"I" => 1, "V" => 5, "X" => 10, "L" => 50, "C" => 100, "D" => 500 }
+
+
+end
---
+++ spec/roman_numerals_spec.rb
@@ -0,0 +1,10 @@
+require 'roman_numerals'
+
+describe 'Numerals' do
+
+  describe 'ROMAN_NUMERALS' do
+    it 'Has an array of hashes with roman numerals stored inside' do
+      it { should_be kind_of(Hash) }
+    end
+  end
+end
